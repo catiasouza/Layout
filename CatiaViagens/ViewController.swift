9ab54ec3917bc2679de721ee81169c902a900d07
Catia Miranda de Souza <catiamiranda@NBBV030098.local>
2019-10-02 16:20:14 -0300

    Customizando a lista

--- CatiaViagens/ViewController.swift
+++ CatiaViagens/ViewController.swift
@@ -10,21 +10,31 @@ import UIKit
 
 class ViewController: UIViewController,UITableViewDataSource {
     
+    @IBOutlet weak var tabelaViagens: UITableView!
+    
+    @IBOutlet weak var viewPacotes: UIView!
+    @IBOutlet weak var viewsHoteis: UIView!
+    
     //criar uma lista
-    let listaViagens:Array<String> = ["Rio de Janeiro","Ceara","Sao Paulo"]
-
+    let listaViagens:Array<Viagem> = ViagemDAO().retornaTodasAsViagens()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.tabelaViagens.dataSource = self
+        self.viewPacotes.layer.cornerRadius = 10
+        self.viewsHoteis.layer.cornerRadius = 10            //configurando botao pacotes q ja esta conectado
     }
     
     //retorna qts elementos vem da lista
-     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return listaViagens.count
     }
     // mostra o conteudo q a celula tem q mostrar"que no caso e o "cell" q e o identifiquer da celula
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        let viagemAtual = listaViagens[indexPath.row]
+        cell.textLabel?.text = viagemAtual.titulo
         
         return cell
     }
